765aee0edf030bfae767e65326eca6d38ef945d6
JH\JH <reach0328@gmail.com>
2017-03-21 11:53:08 +0900

    sharedpreference를 활용한 저장

--- app/src/main/java/com/jpark/sharedpreference/MainActivity.java
+++ app/src/main/java/com/jpark/sharedpreference/MainActivity.java
@@ -1,5 +1,7 @@
 package com.jpark.sharedpreference;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -24,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
         if("false".equals(propertyUtil.getProperty("firstOpen"))){
             layout.setVisibility(View.GONE);
         }
+        //세팅된 값을 가져와서 화면에 뿌린다.
+        loadSetting();
     }
     public void closeHelp(View view) {
         layout.setVisibility(View.GONE);
@@ -31,8 +35,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void saveSetting(View view) {
+        // Preference 생성
+        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+        // editor를 가져와서 추가(값을 입력하기위해서는 editor로 가져와야한다)
+        SharedPreferences.Editor editor = sharedPref.edit();
 
+        editor.putString("email",edit_email.getText().toString());
+        editor.putBoolean("suffle",switch_suffle.isChecked());
+        editor.commit();
     }
-
-
+    public void loadSetting() {
+        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+        String email = sharedPref.getString("email",null);
+        boolean check = sharedPref.getBoolean("suffle", false);
+        edit_email.setText(email);
+        switch_suffle.setChecked(check);
+    }
 }
